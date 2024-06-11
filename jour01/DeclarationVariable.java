05f2ab8317fc460ed3f5e43ff7a636d7ea11cf1d
Sluchai40 <mabbolos8@gmail.com>
2024-06-11 21:50:02 +0200

    [CREATE] jour 01 job DeclarationVariable

---
+++ jour01/DeclarationVariable.java
@@ -0,0 +1,15 @@
+package jour01;
+/*package jour01;*/
+
+/*
+ * This is a simple program to declare three variables in Java
+* It declares three integers and assigns the value 0 to all three variables
+ */
+
+public class DeclarationVariable {
+    public static void main(String args[]) {
+        int x, y, z; // Declare three integers
+        x = y = z = 0; // Assign the value 0 to all three variables
+        System.out.println("x: " + x + ", y: " + y + ", z: " + z);
+    }
+}
