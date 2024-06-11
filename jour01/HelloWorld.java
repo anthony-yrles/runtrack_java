af16a6f88d3e0425a2e937ae344dfebe5d8b4c30
Sluchai40 <mabbolos8@gmail.com>
2024-06-11 21:00:18 +0200

    [CREATE] Jour 01 job HelloWorld avec les commentaires

--- jour01/HelloWorld.java
+++ jour01/HelloWorld.java
@@ -1,7 +1,12 @@
 package jour01;
 
+/*
+ * This is a simple Hello World program in Java
+ * It prints "Hello World" to the console
+ */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Prints "Hello World" to the console
+        System.out.println("Hello World"); // Display the string by using the println method
     }
 }
