5f129ead64df275a91023ec03d35af4cbd314726
Sluchai40 <mabbolos8@gmail.com>
2024-06-11 22:18:01 +0200

    [CREATE] jour 01 Addition

---
+++ jour01/Addition.java
@@ -0,0 +1,17 @@
+package jour01;
+
+/*
+ * This is a simple program to perform some calculations in Java
+ */
+
+public class Addition {
+    public static void main(String[] args) {
+
+        // Declare two integers and assign the values 3 and 73 to them
+        int num1 = 3;
+        int num2 = 73;
+
+        // Print the sum of the two numbers
+        System.out.println("La division exacte est égal à " + (num1 / num2) + "\n");
+    }
+}
--- jour01/ExerciceVariables.java
+++ jour01/ExerciceVariables.java
@@ -1,14 +1,18 @@
 package jour01;
 
+/*
+ * This is a simple program to swap the values of two variables in Java
+ */
+
 public class ExerciceVariables {
     public static void main(String[] args) {
         
-        int num1 = 5;
-        int num2 = 10;
+        int num1 = 5; // Declare an integer and assign the value 5 to it
+        int num2 = 10; // Declare an integer and assign the value 10 to it
         System.out.println("num1 = " + num1 + " et num2 = " + num2);
-        int num = num1;
-        num1 = num2;
-        num2 = num;
+        int num = num1; // Declare an integer and assign the value of num1 to it
+        num1 = num2; // Assign the value of num2 to num1
+        num2 = num; // Assign the value of num to num2
         System.out.println("num1 = " + num1 + " et num2 = " + num2);
     }
 }
