18ea85201ff52e96787b39c08664cc1f80ae84c2
Sluchai40 <mabbolos8@gmail.com>
2024-06-11 22:11:37 +0200

    [CREATE] jour 01 Calcul

---
+++ jour01/Calcul.java
@@ -0,0 +1,16 @@
+package jour01;
+
+/*
+ * This is a simple program to perform some calculations in Java
+ * It declares an integer and performs some calculations on it
+ */
+public class Calcul {
+    public static void main(String[] args) {
+        int nombre = 7; // Declare an integer and assign the value 7 to it and print it
+        System.out.println("Le nombre est " + nombre + "\n");
+        nombre += 3; // Add 3 to the number and print it
+        System.out.println("Le nombre est " + nombre + "\n");
+        nombre *= 5; // Add 5 to the number and print it
+        System.out.println("Le nombre est " + nombre + "\n");
+    }
+}
--- jour01/DeclarationVariable.java
+++ jour01/DeclarationVariable.java
@@ -1,5 +1,4 @@
 package jour01;
-/*package jour01;*/
 
 /*
  * This is a simple program to declare three variables in Java
@@ -7,7 +6,7 @@ package jour01;
  */
 
 public class DeclarationVariable {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         int x, y, z; // Declare three integers
         x = y = z = 0; // Assign the value 0 to all three variables
         System.out.println("x: " + x + ", y: " + y + ", z: " + z);
