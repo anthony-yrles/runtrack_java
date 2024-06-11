b2de95e620f7ac6b57a8a5aa3fef9b0fdcaeb075
Sluchai40 <mabbolos8@gmail.com>
2024-06-11 22:22:31 +0200

    [CREATE] jour 01 Facture

---
+++ jour01/Facture.java
@@ -0,0 +1,18 @@
+package jour01;
+
+/*
+ * This is a simple program to calculate the total and tax of a product in Java
+ */
+public class Facture {
+    public static void main(String[] args) {
+
+        double prix = 49.99;
+        int quantite = 3;
+        double tarifHT = prix * quantite; // Declare a double and assign the value of prix * quantite to it
+        System.out.println("Le total est " + tarifHT + "\n");
+
+        double taxe = 0.20; // Declare a double and assign the value 0.20 to it
+        double tarifTTC = tarifHT + tarifHT * taxe; // Declare a double and assign the value of tarifHT + tarifHT * taxe to it
+        System.out.println("La taxe est " + tarifTTC + "\n");
+    }
+}
