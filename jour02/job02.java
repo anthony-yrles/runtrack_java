51f2f3fa2c81333d674087ada4af8f4b1f5545f3
Sluchai40 <mabbolos8@gmail.com>
2024-06-11 23:43:53 +0200

    [CREATE] jour 02 job02

---
+++ jour02/job02.java
@@ -0,0 +1,15 @@
+package jour02;
+
+/*
+ * Write a program that displays the numbers from 100 to 1 on the screen.
+ */
+
+public class job02 {
+    public static void main(String[] args) {
+
+        // Loop from 1 to 100
+        for (int i = 100; i > 0; i--) {
+            System.out.println(i); // Output the result of each iteration
+        }
+    }
+}
