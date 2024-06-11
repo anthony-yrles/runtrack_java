3fa3e32e872e7fa7cec4f707fe806fc2137903ec
Sluchai40 <mabbolos8@gmail.com>
2024-06-11 22:23:50 +0200

    [CREATE] jour 01 Facture

--- jour01/Facture.java
+++ jour01/Facture.java
@@ -11,8 +11,11 @@ public class Facture {
         double tarifHT = prix * quantite; // Declare a double and assign the value of prix * quantite to it
         System.out.println("Le total est " + tarifHT + "\n");
 
-        double taxe = 0.20; // Declare a double and assign the value 0.20 to it
-        double tarifTTC = tarifHT + tarifHT * taxe; // Declare a double and assign the value of tarifHT + tarifHT * taxe to it
+        double tva = 0.20; // Declare a double and assign the value 0.20 to it
+        double taxe = tarifHT * tva; // Declare a double and assign the value of tarifHT * tva to it
+        System.out.println("La taxe est " + taxe + "\n");
+
+        double tarifTTC = tarifHT + taxe; // Declare a double and assign the value of tarifHT + tarifHT * taxe to it
         System.out.println("La taxe est " + tarifTTC + "\n");
     }
 }
