d4f9d3faff90e9f143649e8567fd3043e26f848a
Sluchai40 <mabbolos8@gmail.com>
2024-06-12 16:26:14 +0200

    [CREATE] jour 03 job2

---
+++ jour03/job02.java
@@ -0,0 +1,12 @@
+package jour03;
+
+public class job02 {
+    public static void main(String[] args) {
+        
+        int[] monTableau = {12,6,76,89};
+
+        for (int i = 0; i < monTableau.length; i++) {
+            System.out.println(monTableau[i]);
+        }
+    }
+}
