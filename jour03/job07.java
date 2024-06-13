40bfbd59a825d97b8e10990188ba6adea52a2aa1
Sluchai40 <mabbolos8@gmail.com>
2024-06-13 08:47:02 +0200

    [CREATE] jour 03 job07

---
+++ jour03/job07.java
@@ -0,0 +1,18 @@
+package jour03;
+
+public class job07 {
+    public static void main(String[] args) {
+        int[][] matrice1 = { {1,2,3} , {4,5,6}, {7,8,9} };
+        int[][] matrice2 = { {10,20,30} , {40,50,60} , {70,80,90} };
+
+        int total = 0;
+
+        for (int i = 0; i < matrice1.length; i++) {
+            for (int j = 0; j < matrice1[i].length; j++) {
+                System.out.print(matrice1[i][j] + " ");
+                total = total + matrice1[i][j] + matrice2[i][j];
+            }
+            }
+        System.out.println("\n" + total);
+    }
+}
